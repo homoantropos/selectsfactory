eff471ea9304b5b090967eb1e434d68b265d11e0
Segii Antropov <homoantropos@gmail.com>
2022-02-06 23:28:56 +0200

    mockDB added

---
+++ src/app/show-room/field-names-choice/field-names-choice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-field-names-choice',
+  templateUrl: './field-names-choice.component.html',
+  styleUrls: ['./field-names-choice.component.css']
+})
+export class FieldNamesChoiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
